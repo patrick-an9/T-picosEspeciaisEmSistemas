bb66965d6999e9f907970462945ee9c05055a709
Auto Logon <autologon@CSED.LAB>
2024-03-11 20:33:27 -0300

    Aula git

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Primeiro programa em C#");
+Console.WriteLine(123456);
+int idade = 50;
+Console.WriteLine("idade: " + idade + "  e alguma coisa");
+
+
+string texto = Console.ReadLine();
+
+Console.WriteLine(texto);
+
